cbbc9729a61c0f66d17425c2ad4ef241bf614072
Erin Stansbury <ekstansbury@gmail.com>
2020-04-03 04:11:47 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,6 +1,7 @@
-class Human < Player
-  def move(board)
-    puts "Where do you want to move? Please enter an input."
-    input = gets.strip
+module Players
+  class Human < Player
+    def move(board)
+      input = gets.strip
+    end
   end
 end
