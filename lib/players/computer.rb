7d930cd6d86fbff48dedfbb56fcb881b078a246e
Erin Stansbury <ekstansbury@gmail.com>
2020-04-03 04:10:52 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,12 +1,13 @@
 module Players
   class Computer < Player
-  def move(board)
-   input = (1 + rand(9)).to_s
+    def move(board)
+      input = (1 + rand(9)).to_s
 
-   if board.valid_move?(input) == false
-    self.move
-   else
-    return input
-   end
+      if board.valid_move?(input) == false
+        self.move
+      else
+        return input
+      end
+    end
   end
 end
