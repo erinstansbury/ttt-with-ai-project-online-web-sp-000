285ff20d6a788a845cb17f7f08f37d7defc55f10
Erin Stansbury <ekstansbury@gmail.com>
2020-04-03 02:55:01 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -70,4 +70,17 @@ class Game
     end
   end
 
+  def turn
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?
+      move
+      display_board
+    else
+      turn
+    end
+
+  end
+
 end
